d026fd51be0c6a3abb3e87aa359ae9ef6734b165
Alexis-Georganopoulos <georganopoulos.alexis@gmail.com>
2023-03-30 22:34:44 +0100

    clarrified some details in control script

--- control/control.m
+++ control/control.m
@@ -1,7 +1,10 @@
 function u = control(q, dq,t)%(t, q, dq, q0, dq0, step_number)
-% You may call control_hyper_parameters and desired_outputs in this
-% function
-% you don't necessarily need to use all the inputs to this control function
+
+% control system for the bipedal walker. In principle it uses
+%impedance control (and passively uses zero dynamics), but the 
+%resulting equations are a modified PD controller. 
+%In this case, impedance/PD control are one and the same
+
 [~, ~, ~, l1, ~, ~, ~] = set_parameters();
 
 J_hat = [l1*cos(q(1)), 0, 0; ...
@@ -10,18 +13,27 @@ J_hat = [l1*cos(q(1)), 0, 0; ...
         
 C = [1, 0, -1; 0, 1, -1];
 
-J_t = (J_hat*pinv(C))';
+%"adjusted" jacobian since our system is MIMO, and we dont use a state-space controller
+J_t = (J_hat*pinv(C))'; 
 
+%% These constants are the main hyper-parameters for the control method
+
+%Desired tracking angles
 theta_ref = pi/12;                  %SLOW pi/12 REF pi/6 = FAST
-
 torso_ref = pi/36;                  %SLOW pi/36 REF pi/20 = FAST
 
+%Impedance parameters
 stiffness_interleg = 25;           %SLOW 25   REF 40 = FAST
 dampening_interleg = 4;            %SLOW 4    REF 2.5 = FAST
+
 stiffness_torso = 75;               %SLOW 75    REF 80 = FAST
 dampening_torso = 4;                %SLOW 4     REF 1 = FAST
+
+%These terms help with stabilisation
 dx_hip_tracking = -5;               %SLOW -5    REF 1 FAST 2.2
 k_hip = 4;                         %SLOW 4    REF 1 FAST 0.5
+
+
 
 dx_hip = l1*cos(q(1))*dq(1);
 F_rabit = k_hip*(dx_hip - dx_hip_tracking);
@@ -34,7 +46,7 @@ F_swing = -stiffness_interleg*(q(2)-q(1)+theta_ref) - ...
 
 F = [F_rabit;F_torso;F_swing];
 
-u = J_t*F;
+u = J_t*F; %ie, the jacobian of the system * force vector = control torques
 
 if(abs(u(1)) > 30)
     u(1) = 30*sign(u(1));
